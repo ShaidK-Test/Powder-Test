04b2447f8df178522b3d2d3b70926c73c469cca0
ShaidK-Test <105226332+ShaidK-Test@users.noreply.github.com>
2022-07-15 14:30:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const core = require('@actions/core');
+//
+//
+//
 const github = require('@actions/github');
+const core   = require('@actions/core');
+
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -12,3 +16,4 @@ try {
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
+}
