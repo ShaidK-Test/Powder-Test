7002e5e23ad25cee6f2ad821c23d325795851ee8
ShaidK-Test <105226332+ShaidK-Test@users.noreply.github.com>
2022-07-24 19:35:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 //
-//
+// File: index.js / Note: Maintain the main entry point for the Snowflake Action  
 //
 
+const package_json = require('./package.json');
 const core = require('@actions/core');
 
-try {
-    const { version } = require("./package.json");
-    core.setOutput("package_version", version);
+async function run() {
+    try {
+        const package_json_property = core.getInput('require_property');
+        core.setOutput("property_${package_json_property}", inputProperty(package_json_property));
+    }
+    catch (err) {
+      core.setFailed(err.message);
+    }
 }
-catch (err) {
-    core.setFailed(error.message);
-}
+
+exports.inputProperty = (arg) => {
+    if (package_json[arg] == undefined) {
+        throw new TypeError("Properties '${arg}' doesn't exist within File 'package.json'", "index.js", "17")
+    }
+
+    return package_json[arg]; 
+};
+  
+run()
+
