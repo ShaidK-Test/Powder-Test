135fdc181ecf5c86b26bae1aedef76245abb7e36
ShaidK-Test <105226332+ShaidK-Test@users.noreply.github.com>
2022-07-25 08:01:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,14 @@
 const package_json = require('./package.json');
 const core = require('@actions/core');
 
+exports.inputProperty = (arg) => {
+    if (package_json[arg] == undefined) {
+        throw new TypeError("Properties '${arg}' doesn't exist within File 'package.json'", "index.js", "17")
+    }
+
+    return package_json[arg]; 
+};
+
 async function run() {
     try {
         const package_json_property = core.getInput('require_property');
@@ -14,14 +22,6 @@ async function run() {
       core.setFailed(err.message);
     }
 }
-
-exports.inputProperty = (arg) => {
-    if (package_json[arg] == undefined) {
-        throw new TypeError("Properties '${arg}' doesn't exist within File 'package.json'", "index.js", "17")
-    }
-
-    return package_json[arg]; 
-};
   
 run()
 
