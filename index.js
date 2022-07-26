d678ed94ffcd42131555b0cc5a28d5229439bf19
ShaidK-Test <105226332+ShaidK-Test@users.noreply.github.com>
2022-07-26 14:24:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const package_json = require('./package.json');
 const core = require('@actions/core');
 
 function getExtensionManifestProperty(arg) {
-    console.log("Executing Snowflake Action to extract following Property: ${arg} " + arg)
+    console.log('Executing Snowflake Action to extract following Property: ${arg} ' + arg)
     if (package_json[arg] == undefined)
         throw new TypeError("Properties '${arg}' doesn't exist within File 'package.json'");
 
@@ -16,7 +16,7 @@ function getExtensionManifestProperty(arg) {
 async function run() {
     try {
         const package_json_property = core.getInput('require_property');        
-        core.setOutput("property_${package_json_property}", getExtensionManifestProperty(package_json_property));
+        core.setOutput('property_${package_json_property}', getExtensionManifestProperty(package_json_property));
     }
     catch (err) {
         core.setFailed(err.message);
